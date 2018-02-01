ca494b337e8216d7de9775c5baaa8b1bf92c8eec
Pedro Arvellos <pedroarvellos@hotmail.com>
2018-02-01 11:29:24 -0200

    Adding breadcrumbs on each page!

---
+++ src/components/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <!-- Mocked values just for showing Vue.js functionalities -->
+        <thumbnail/>
+        <thumbnail/>
+        <thumbnail/> 
+    </div>
+</template>
+<script>
+import Thumbnail from "../shared/Thumbnail.vue";
+
+export default {
+  components: {
+    "thumbnail": Thumbnail
+  }
+};
+</script>
+<style scoped>
+
+</style>
